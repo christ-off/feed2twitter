a0456549d240e65639c5218fd9b83e3d88a026e5
Christ-OFF <4630125+Christ-OFF@users.noreply.github.com>
2019-09-13 00:01:27 +0200

    CHANGE - WIP Tweet

--- src/output/tweet.js
+++ src/output/tweet.js
@@ -2,6 +2,19 @@
 
 // http module doesn't provide async/promise
 const disabler = require('../utils/console-disabler');
+const twit = require('twit');
+
+/**
+ * Init Twitter LIB
+ */
+let t = new Twit({
+  consumer_key:process.env['CONSUMER_KEY'],
+  consumer_secret:process.env['CONSUMER_SECRET'],
+  access_token:process.env['ACCESS_TOKEN'],
+  access_token_secret:process.env['ACCESS_TOKEN_SECRET'],
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+});
 
 exports.tweet = async (entry) => {
 
